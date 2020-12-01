57a62db11129ae99a16656d8f9b7e3fef4b3944e
ZooooooooZ <69062609+ZooooooooZ@users.noreply.github.com>
2020-12-01 14:19:05 +0300

    zoom 9

--- event_reverse_geocode.js
+++ event_reverse_geocode.js
@@ -6,7 +6,7 @@ function init() {
         //Создаем карту (объект класса Map)
         myMap = new ymaps.Map('map', {
             center: [55.753994, 37.622093],
-            zoom: 1
+            zoom: 9
         }, {
             searchControlProvider: 'yandex#search'
         });
